2431c549242223cb9c404764316943f2723f9f7d
Suraj P <surajprajapati8520@gmail.com>
2024-09-30 20:57:12 +0530

    completed IPA_1

--- IPA _01/CourseProgram.java
+++ IPA _01/CourseProgram.java
@@ -41,13 +41,25 @@ class CourseProgram{
         sc.nextLine();
         // sc.nextLine();
         String admin = sc.nextLine();
+        int handson = sc.nextInt();
 
+        // To get the courses which are less than handson
         int avg = findAvgOfQuizByAdmin(courses, admin);
         if(avg == 0){
-            System.out.println("No course found! " + admin);
+            System.out.println("No course found!");
         }else{
             System.out.println(avg);
         }
+
+        Course[] crs = sortCourseByHandsOn(courses, handson);
+        if(crs.length == 0){
+            System.out.println("No courses found with mentioned attributes.");
+        }else{
+            for(Course cs: crs){
+                System.out.println(cs.getCourseName());
+            }
+        }
+
         sc.close();
     }
 
@@ -70,7 +82,37 @@ class CourseProgram{
     }
 
     public static Course[] sortCourseByHandsOn(Course[] crs, int handson){
+
+        int count = 0;
+
+        // Get the count of courses which are less than handson
+        for(Course cs: crs){
+            int crsHandson = cs.getHandson();
+            if(crsHandson < handson) count++;
+        }
+
+        Course[] result = new Course[count];
         
-        return new Course[0];
+        // add the courses which are less than handson in new array
+        int index = 0;
+        for(Course cs: crs){
+            int crsHandson = cs.getHandson();
+            if(crsHandson < handson){
+                result[index] = cs;
+                index++;
+            }
+        }
+
+        // Now sort those courses in ascending order
+        for(int i = 0; i < count - 1; i++){
+            for(int j = 0; j < count-i-1; j++){
+                if(result[j].getHandson() > result[j+1].getHandson()){
+                    Course temp = result[j];
+                    result[j] = result[j+1];
+                    result[j+1] = temp;
+                }
+            }
+        }
+        return result;
     }
 }
